259e388a38d04432856a3c9f9755c2fd04f9c89f
migsii <migsiimanalastas@gmail.com>
2026-06-03 13:08:26 +0800

    Database connection

---
+++ lib/dbConnect.ts
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI || "";
+
+if (!MONGODB_URI) {
+  throw new Error(
+    "Please define the MONGODB_URI environment variable inside .env.local",
+  );
+}
+
+interface MongooseCache {
+  conn: typeof mongoose | null;
+  promise: Promise<typeof mongoose> | null;
+}
+
+declare global {
+  var mongoose: MongooseCache | undefined;
+}
+
+let cached = globalThis.mongoose;
+
+if (!cached) {
+  cached = globalThis.mongoose = { conn: null, promise: null };
+}
+
+async function dbConnect() {
+  if (cached!.conn) return cached!.conn;
+
+  if (!cached!.promise) {
+    cached!.promise = mongoose.connect(MONGODB_URI).then((m) => m);
+  }
+
+  cached!.conn = await cached!.promise;
+  return cached!.conn;
+}
+
+export default dbConnect;
